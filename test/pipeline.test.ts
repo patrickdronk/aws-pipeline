d11acef49d1997135207465ee461a73f61aaa1bc
Patrick Dronk <patrickdronk@me.com>
2021-12-11 20:00:00 +0100

    final solution

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,17 +1,17 @@
-// import { Template } from '@aws-cdk/assertions';
-// import * as cdk from '@aws-cdk/core';
-// import * as Pipeline from '../lib/pipeline-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  SynthUtils,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Pipeline from "../lib/pipeline-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/pipeline-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("Pipeline Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Pipeline.PipelineStack(app, "MyTestStack");
+  // THEN
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
